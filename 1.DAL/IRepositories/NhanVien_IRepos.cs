b436ddc7e2434ddcdf3e008e7c794c4b7b345b89
Vũ Tuấn Anh <tuananh26ttvt@gmail.com>
2023-11-23 13:44:35 +0700

    update repos

--- 1.DAL/IRepositories/NhanVien_IRepos.cs
+++ 1.DAL/IRepositories/NhanVien_IRepos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _1.DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace _1.DAL.IRepositories
 {
     public interface NhanVien_IRepos
     {
+        List<NhanVien> GetAllNhanVien();
+        List<NhanVien> GetNVByName(string name);
+        bool CreateNhanVien(NhanVien nhanVien);
+        bool UpdateNhanVien(NhanVien nhanVien);
     }
 }
