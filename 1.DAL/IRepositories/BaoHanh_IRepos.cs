2c539cc4e2b28de6677eded5e25982ee8b2e05db
Hoang Viet Anh <anhhvph31156@fpt.edu.vn>
2023-11-26 12:25:35 +0700

    Tạo BaoHanh,DoiTra_IRepos + BaoHanh,DoiTraRepos

--- 1.DAL/IRepositories/BaoHanh_IRepos.cs
+++ 1.DAL/IRepositories/BaoHanh_IRepos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _1.DAL.Model2s;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace _1.DAL.IRepositories
 {
     public interface BaoHanh_IRepos
     {
+        List<BaoHanh> GetBaoHanh(string input);
+
+        bool CreateBaoHanh(BaoHanh baoHanh);
     }
 }
--- 1.DAL/Repositories/BaoHanhRepos.cs
+++ 1.DAL/Repositories/BaoHanhRepos.cs
@@ -1,12 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using _1.DAL.IRepositories;
+using _1.DAL.Model2s;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
 
 namespace _1.DAL.Repositories
 {
-    public class BaoHanhRepos
+    public class BaoHanhRepos : BaoHanh_IRepos
     {
+        OnlyFansContext _context = new OnlyFansContext();
+        public BaoHanhRepos()
+        {
+            
+        }
+
+        public BaoHanhRepos(OnlyFansContext context)
+        {
+            _context = context;
+        }
+
+        public List<BaoHanh> GetBaoHanh(string input)
+        {
+            if (input == null)
+            {
+                return _context.BaoHanhs.ToList();
+            }
+            else
+            {
+                var BaoHanh1 = _context.BaoHanhs
+                .Include(a => a.LichSuBaoHanhs)
+                .ThenInclude(b => b.IdHoaDonCtNavigation)
+                .Where(a => a.LichSuBaoHanhs.Any(b => b.IdHoaDonCtNavigation != null))
+                .Where(a => a.LichSuBaoHanhs.Any(a => a.IdHoaDonCtNavigation.IdBienTheNavigation.MaBienThe.Contains(input)))
+                .ToList();
+
+
+                var BaoHanh2 = (from a in _context.BaoHanhs
+                               join b in _context.LichSuBaoHanhs on a.IdBaoHanh equals b.IdBaoHanh
+                               join c in _context.HoaDonCts on b.IdHoaDonCt equals c.IdHoaDonCt
+                               where c.IdBienTheNavigation.MaBienThe.Contains(input)
+                               select a).ToList();
+
+
+                //return BaoHanh1;
+                return BaoHanh2;
+            }
+        }
+
+        public bool CreateBaoHanh(BaoHanh baoHanh)
+        {
+            try
+            {
+                _context.BaoHanhs.Add(baoHanh);
+                _context.SaveChanges();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        
     }
 }
